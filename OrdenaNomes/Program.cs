f6f4c003bb055cbe1a4394a40d34a3dd908b0410
Idevaldo Urias <idevaldorodrigues@gmail.com>
2023-08-27 20:04:36 -0300

    Adicionar arquivos de projeto.

---
+++ OrdenaNomes/Program.cs
@@ -0,0 +1,23 @@
+﻿string path = @"C:\Users\ideva\source\repos\OrdenaNomes\OrdenaNomes\nomes.txt";
+
+try
+{
+    using (StreamReader sr = File.OpenText(path))
+    {
+        List<string> list = new List<string>();
+        while (!sr.EndOfStream)
+        {
+            list.Add(sr.ReadLine());
+        }
+        list.Sort();
+        foreach (string line in list)
+        {
+            Console.WriteLine(line);
+        }
+    }
+} 
+catch (IOException e)
+{
+    Console.WriteLine("An error ocurred: ");
+    Console.WriteLine(e.Message);
+}
